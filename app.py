cc340b2a4a5a2ae911532c6983e9801719fd7559
d-broder <daniel_sendreti@hotmail.com>
2023-10-17 14:26:32 -0300

    Implementação do Flask

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask("__name__")
+
+
+@app.route("/")
+def home():
+    # return "<h1>Olá Flask!</h1>"
+    return render_template("index.html")
+
+
+@app.route("/quemsomos")
+def quem():
+    return render_template("quemsomos.html")
+
+
+@app.route("/contatos")
+def contato():
+    return render_template("contatos.html")
